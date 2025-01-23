7ae6f62e73330867d3f7623380b28beb39c2135d
Andrew Hodgkinson <andrew@rowing.org.uk>
2025-01-23 13:36:15 +1300

    Rationalise config and use shared views

--- Gemfile
+++ Gemfile
@@ -48,12 +48,23 @@ gem 'acts_as_list', '~> 1.2'
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
-  gem 'debug'
+  gem 'debug', platforms: %i[ mri windows ]
 end
 
 group :development do
+  gem 'error_highlight', '>= 0.4.0', platforms: [:ruby]
+
   # Use console on exceptions pages [https://github.com/rails/web-console]
+  #
   gem 'web-console'
+
+  # Be able to run 'bin/dev'
+  #
+  gem 'foreman'
+
+  # E-mail inspection.
+  #
+  gem 'mailcatcher'
 end
 
 group :test do
--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ module Beast
 
     # Initialize configuration defaults for originally generated Rails version.
     #
-    config.load_defaults 7.1
+    config.load_defaults 8.0
 
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
@@ -27,17 +27,40 @@ module Beast
     #
     config.autoload_lib(ignore: %w(assets tasks))
 
-    # Permitted hosts.
+    # Configuration for the application, engines, and railties goes here.
     #
-    config.hosts << "epsilon.arachsys.com"
+    # These settings can be overridden in specific environments using the files
+    # in config/environments, which are processed later.
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration can go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and any gems in your application.
-
-    config.time_zone                      = 'UTC'
+    config.time_zone = 'UTC'
     config.active_record.default_timezone = :utc
+
+    # Add the shared ROOL view components.
+    #
+    shared_views_path = if ENV['SHARED_VIEWS_PATH'].blank?
+      Rails.root.join('..', 'common', 'views')
+    else
+      ENV['SHARED_VIEWS_PATH']
+    end
+    config.paths['app/views'].unshift(shared_views_path)
+
+    # If running in a deployed environment, allow requests to Epsilon. Send
+    # e-mail via Beta, which is on the same local network.
+    #
+    if Socket.gethostname == 'epsilon'
+      config.hosts << "epsilon.arachsys.com"
+
+      config.action_mailer.delivery_method = :smtp
+      config.action_mailer.smtp_settings = {
+        address:        'beta.arachsys.com',
+        port:           25,
+        domain:         'epsilon.arachsys.com',
+        user_name:      nil,
+        password:       nil,
+        authentication: nil,
+        enable_starttls_auto: true
+      }
+    end
 
   end
 end
